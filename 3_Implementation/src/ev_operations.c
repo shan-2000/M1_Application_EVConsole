c0e86178aa55350bce23bb0f4efac2a06ae30879
shan-2000 <45603597+shan-2000@users.noreply.github.com>
2021-11-24 14:26:57 +0530

    Functions operations added

--- 3_Implementation/src/ev_operations.c
+++ 3_Implementation/src/ev_operations.c
@@ -1,25 +1,148 @@
 #include <ev_operations.h>
 
 
-int add(int operand1, int operand2)
+int StartVehicle(int Start)//Start takes 1 or 0 where 1 means vehicle is started and 0 meand vehicle is off
 {
-    return operand1 + operand2;
+   printf("\n Car is started \n");
+   return Start;
 }
 
-int subtract(int operand1, int operand2)
+int StopVehicle(int WRPM, bool Stop)//Start takes 1 or 0 where 1 means vehicle is started and 0 meand vehicle is off
 {
-    return operand1 - operand2;
+   printf("\nCar is stopped\n");
+   if(WRPM !=0)
+   {
+      printf("CAUTION...! Please apply hand break");
+   }
+   return Stop;
 }
 
-int multiply(int operand1, int operand2)
+
+float Fspeed(int WRPM, char Gear)
 {
-    return operand1 * operand2;
+   if (Gear == 'F')
+   {
+      float FVehicleSpeed = 0.1885 * WRPM * radius;
+      printf("%0.2f kmph", FVehicleSpeed);
+      //return FVehicleSpeed; //kmph
+      if (FVehicleSpeed > 40)
+      {
+         printf(" CAUTION...!  The Speed if the vehicle is HIGH");
+      }
+      else if(FVehicleSpeed< 40)
+      {
+         printf("\nThe Speed is in the limit");
+      }
+      return FVehicleSpeed;
+   }
+
+   
+}
+//function for Reverse speed calculation
+float Rspeed(int WRPM, char Gear)
+{
+   if (Gear == 'R' && WRPM<=380)//Maximum rever speed is 380RPM
+   {
+      float RVehicleSpeed = 0.1885 * WRPM * radius;
+      printf("%0.2f Rspeed", RVehicleSpeed);
+      printf("\n The vehicle is ");
+      return RVehicleSpeed;
+   }
+   
+}
+char Neutral(char Gear)
+{
+   if (Gear == 'N' )
+   {
+      float NVehicleSpeed = 0.0;
+      printf("\nThe vehicle is in neutral ");
+   }
+   
+}
+float ForceAndTorqueCurrent(int WRPM, char Gear)
+{
+   if (Gear == 'F' || Gear == 'R')
+   {
+   
+      float LSpeed = 0.1047 * WRPM * radius;
+      printf("speed in m/s : %0.2f\n",LSpeed);
+      float Acceleration = LSpeed/accelerationTiming;
+      printf("%0.2f Accel\n", Acceleration);
+      float Force = Mass * Acceleration;
+      printf("%0.2f force\n", Force);
+
+      float Torque = Force * radius;
+      float power = (2 * pi * WRPM * Torque) / 60;
+      float Current = power / Voltage;
+      float StateOfCharge = Current/CurrentCapacity;
+      printf("%0.2f %0.2f",Current, StateOfCharge );
+      printf("%0.2f torque\n", Torque);
+
+   }
+   else
+   {
+      printf("vehicle is in neutral"); //for neural
+   }
+  
+}
+char GearIndicator(char Gear)
+{
+   switch (Gear)
+   {
+   case 'F':
+   {
+      return Gear;
+      break;
+   }
+   case 'R':
+   {
+      return Gear;
+      break;
+   }
+   default:
+   {
+   if(Gear != 'F' && Gear !='R' && Gear =='N')
+   {
+      return Gear;
+      break;
+   }
+   else
+   {
+      printf("\nError - Incorrect input");
+   }
+   }
+   }
+   
 }
 
-int divide(int operand1, int operand2)
+int MotorRPM(int WRPM)
 {
-    if(0 == operand2)
-        return 0;
-    else
-        return operand1 / operand2;
+   int MRPM = WRPM * gearRatio;
+   printf("%d", MRPM);
+   return MRPM;
+}
+
+void TemperatureIndicator(float Temp)
+{
+  if (Temp<=70)
+  {
+     printf("The system temperature is %0.2f Degree /n", Temp);
+  }
+  else if(Temp>70)
+  {
+     printf("CAUTION...! %0.2f is High Temperature", Temp);
+  }  
+}
+
+void ParkingSensor(float ParkingDistance)
+{ 
+   printf("%0.2f", ParkingDistance);
+   if(ParkingDistance <=2)
+   {
+      printf("CAUTION...! Parking Alarm");
+   }
+   else
+   {
+      printf("No obstacle");
+   }
 }
