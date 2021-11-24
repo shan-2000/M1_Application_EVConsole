294d86b7d85470ac1780a1d100f07b29b2b42be6
shan-2000 <45603597+shan-2000@users.noreply.github.com>
2021-11-24 23:16:52 +0530

    2 functions test case added

--- 3_Implementation/test/test_ev_operations.c
+++ 3_Implementation/test/test_ev_operations.c
@@ -2,15 +2,15 @@
 #include <ev_operations.h>
 
 /* Modify these two lines according to the project */
-#include <calculator_operations.h>
-#define PROJECT_NAME    "Calculator"
+#include <ev_operations.h>
+#define PROJECT_NAME    "EVConsole"
 
 /* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
-
+// void test_add(void);
+// void test_subtract(void);
+// void test_multiply(void);
+// void test_divide(void);
+float Fspeed(void);
 /* Required by the unity test framework */
 void setUp(){}
 /* Required by the unity test framework */
@@ -23,40 +23,52 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
+  RUN_TEST(test_Fspeed);
+//   RUN_TEST(test_subtract);
+//   RUN_TEST(test_multiply);
+//   RUN_TEST(test_divide);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
+
+
+void test_Fspeed(void) {
+  TEST_ASSERT_EQUAL(10.53 , Fspeed(200, 'F'));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(15000, add(7500, 7500));
+  TEST_ASSERT_EQUAL(10.53 , Fspeed(300, 'F'));
 }
 
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
+
+void test_Rspeed(void) {
+  TEST_ASSERT_EQUAL(10.53 , Rspeed(200, 'R'));
   
   /* Dummy fail*/
-  TEST_ASSERT_EQUAL(100, subtract(1000, 900));
+  TEST_ASSERT_EQUAL(10.53 , Rspeed(300, 'R'));
 }
 
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(10, multiply(2, 5));
-}
 
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
+
+// void test_subtract(void) {
+//   TEST_ASSERT_EQUAL(-3, subtract(0, 3));
   
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, divide(2, 2));
-}
+//   /* Dummy fail*/
+//   TEST_ASSERT_EQUAL(100, subtract(1000, 900));
+// }
+
+// void test_multiply(void) {
+//   TEST_ASSERT_EQUAL(0, multiply(1, 0));
+  
+//   /* Dummy fail*/
+//   TEST_ASSERT_EQUAL(10, multiply(2, 5));
+// }
+
+// void test_divide(void) {
+//   TEST_ASSERT_EQUAL(0, divide(1, 0));
+  
+//   /* Dummy fail*/
+//   TEST_ASSERT_EQUAL(1, divide(2, 2));
+// }
