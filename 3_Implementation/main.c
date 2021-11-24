825a9985b6ea8e286c9104ac072d7b5a058d584f
shan-2000 <45603597+shan-2000@users.noreply.github.com>
2021-11-24 14:34:52 +0530

    Main.c is updated

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,8 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<stdbool.h>
+#include <ev_operations.h>
 
+/* Status of the operation requested */
+#define VALID   (1)
+#define INVALID (0)
+
+/* Calculator operation requested by user*/
+unsigned int ev_operation = 0;
+
+/* Operands on which calculation is performed */
 //Global Variable
 int gearRatio = 12;
 float radius = 0.2794;
@@ -14,19 +19,12 @@ int CurrentCapacity = 100;
 float Torque;
 
 
-//function declarations
-int StartVehicle(int Start);
-int StopVehicle(int WRPM, bool Stop);
-float Fspeed(int WRPM, char Gear);
-float Rspeed(int WRPM, char Gear);
-char Neutral(char Gear);
-float ForceAndTorqueCurrent(int WRPM, char Gear);
-char GearIndicator(char Gear);
-int MotorRPM(int WRPM);
-void TemperatureIndicator(float Temp);
-void ParkingSensor(float ParkingDistance);
+/* Display the menu of operations supported */
 
-//main function
+/* Verifies the requested operations validity */
+
+
+/* Start of the application */
 void main()
 {
    int VehicleCondition, WheelSpeed, Temperature;
@@ -90,150 +88,4 @@ void main()
       StopVehicle(WheelSpeed,VehicleCondition);
    }
   
-}
-
-int StartVehicle(int Start)//Start takes 1 or 0 where 1 means vehicle is started and 0 meand vehicle is off
-{
-   printf("\n Car is started \n");
-   return Start;
-}
-
-int StopVehicle(int WRPM, bool Stop)//Start takes 1 or 0 where 1 means vehicle is started and 0 meand vehicle is off
-{
-   printf("\nCar is stopped\n");
-   if(WRPM !=0)
-   {
-      printf("CAUTION...! Please apply hand break");
-   }
-   return Stop;
-}
-
-
-float Fspeed(int WRPM, char Gear)
-{
-   if (Gear == 'F')
-   {
-      float FVehicleSpeed = 0.1885 * WRPM * radius;
-      printf("%0.2f kmph", FVehicleSpeed);
-      //return FVehicleSpeed; //kmph
-      if (FVehicleSpeed > 40)
-      {
-         printf(" CAUTION...!  The Speed if the vehicle is HIGH");
-      }
-      else if(FVehicleSpeed< 40)
-      {
-         printf("\nThe Speed is in the limit");
-      }
-      return FVehicleSpeed;
-   }
-
-   
-}
-//function for Reverse speed calculation
-float Rspeed(int WRPM, char Gear)
-{
-   if (Gear == 'R' && WRPM<=380)//Maximum rever speed is 380RPM
-   {
-      float RVehicleSpeed = 0.1885 * WRPM * radius;
-      printf("%0.2f Rspeed", RVehicleSpeed);
-      printf("\n The vehicle is ");
-      return RVehicleSpeed;
-   }
-   
-}
-char Neutral(char Gear)
-{
-   if (Gear == 'N' )
-   {
-      float NVehicleSpeed = 0.0;
-      printf("\nThe vehicle is in neutral ");
-   }
-   
-}
-float ForceAndTorqueCurrent(int WRPM, char Gear)
-{
-   if (Gear == 'F' || Gear == 'R')
-   {
-   
-      float LSpeed = 0.1047 * WRPM * radius;
-      printf("speed in m/s : %0.2f\n",LSpeed);
-      float Acceleration = LSpeed/accelerationTiming;
-      printf("%0.2f Accel\n", Acceleration);
-      float Force = Mass * Acceleration;
-      printf("%0.2f force\n", Force);
-
-      float Torque = Force * radius;
-      float power = (2 * pi * WRPM * Torque) / 60;
-      float Current = power / Voltage;
-      float StateOfCharge = Current/CurrentCapacity;
-      printf("%0.2f %0.2f",Current, StateOfCharge );
-      printf("%0.2f torque\n", Torque);
-
-   }
-   else
-   {
-      printf("vehicle is in neutral"); //for neural
-   }
-  
-}
-char GearIndicator(char Gear)
-{
-   switch (Gear)
-   {
-   case 'F':
-   {
-      return Gear;
-      break;
-   }
-   case 'R':
-   {
-      return Gear;
-      break;
-   }
-   default:
-   {
-   if(Gear != 'F' && Gear !='R' && Gear =='N')
-   {
-      return Gear;
-      break;
-   }
-   else
-   {
-      printf("\nError - Incorrect input");
-   }
-   }
-   }
-   
-}
-
-int MotorRPM(int WRPM)
-{
-   int MRPM = WRPM * gearRatio;
-   printf("%d", MRPM);
-   return MRPM;
-}
-
-void TemperatureIndicator(float Temp)
-{
-  if (Temp<=70)
-  {
-     printf("The system temperature is %0.2f Degree /n", Temp);
-  }
-  else if(Temp>70)
-  {
-     printf("CAUTION...! %0.2f is High Temperature", Temp);
-  }  
-}
-
-void ParkingSensor(float ParkingDistance)
-{ 
-   printf("%0.2f", ParkingDistance);
-   if(ParkingDistance <=2)
-   {
-      printf("CAUTION...! Parking Alarm");
-   }
-   else
-   {
-      printf("No obstacle");
-   }
 }
