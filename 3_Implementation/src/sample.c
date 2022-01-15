b48dfd53f9737fcd9c675777de9637c70b3d0e8d
shan-2000 <45603597+shan-2000@users.noreply.github.com>
2022-01-15 19:06:44 +0530

    sample added

---
+++ 3_Implementation/src/sample.c
@@ -0,0 +1,25 @@
+//x+y
+
+int add(x,y)
+{
+  int z;
+  z = x+y;
+  printf("%d", z)
+  return 0;
+}
+
+void main()
+{
+  printf("Enter the numb: ");
+  scanf("%d", a);
+    printf("Enter the numb: ");
+  scanf("%d", b);
+  add(a,b);
+  add(1,3);
+  add(1,5);
+//   int s;
+//   s = add(x,y);
+//   printf("%d", s);
+}
+
+  
